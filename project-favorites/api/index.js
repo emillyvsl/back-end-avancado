b6327684753244ff837852b42e00ad718e2acdca
Emilly Silva <emillysilva.18@outlook.com>
2023-07-10 16:32:34 -0500

    Revert "Update parcial"
    This reverts commit b39766a32d4539928664d8a1876993ed4d4458d1.

--- project-favorites/api/index.js
+++ project-favorites/api/index.js
@@ -15,38 +15,21 @@ function writeFile(cb) {
   );
 }
 
-http.createServer((req, res) => {
-  const { name, url, del, update } = URL.parse(req.url, true).query;
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Adiciona o cabeçalho CORS
 
+http.createServer((req, res) => {
+  const { name, url, del } = URL.parse(req.url, true).query;
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Adiciona o cabeçalho CORS
   if (!name || !url) {
     return res.end(JSON.stringify(data));
   }
 
   if (del) {
-    // Remove a URL com base no parâmetro 'del'
-    data.urls = data.urls.filter(item => item.url !== del);
+    data.urls = data.urls.filter(item => item.url !== url);
     writeFile(message => {
       res.end(message);
     });
-  } else if (update) {
-    // Atualiza uma URL existente com base nos parâmetros fornecidos
-    const index = data.urls.findIndex(item => item.url === update);
-    if (index !== -1) {
-      if (name) {
-        data.urls[index].name = name;
-      }
-      if (url) {
-        data.urls[index].url = url;
-      }
-      writeFile(message => {
-        res.end(message);
-      });
-    } else {
-      res.end('URL não encontrada');
-    }
   } else {
-    // Adiciona uma nova URL ao array 'data.urls'
+    
     data.urls.push({ name, url });
     writeFile(message => {
       res.end(message);
